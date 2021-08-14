9d009c3d6606f16162fcb68b9f84b3d147ed55f6
u-one <backflip270bb@gmail.com>
2021-08-14 18:01:28 +0900

    Initialized with SpringInitializr

---
+++ src/main/java/net/uoneweb/tokyotrainnow/TokyotrainnowApplication.java
@@ -0,0 +1,13 @@
+package net.uoneweb.tokyotrainnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokyotrainnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokyotrainnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/uoneweb/tokyotrainnow/TokyotrainnowApplicationTests.java
@@ -0,0 +1,13 @@
+package net.uoneweb.tokyotrainnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokyotrainnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
