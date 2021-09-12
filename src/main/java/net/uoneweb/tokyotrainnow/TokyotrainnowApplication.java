6a628cd2d43aec4ea9a5f4f3e2e92ab56cbc7d0f
u-one <backflip270bb@gmail.com>
2021-09-13 00:27:29 +0900

    Add script for environment

--- src/main/java/net/uoneweb/tokyotrainnow/TokyotrainnowApplication.java
+++ src/main/java/net/uoneweb/tokyotrainnow/TokyotrainnowApplication.java
@@ -1,13 +1,28 @@
 package net.uoneweb.tokyotrainnow;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 
+import java.util.Arrays;
+
+@Slf4j
 @SpringBootApplication
 public class TokyotrainnowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TokyotrainnowApplication.class, args);
-	}
+    @Autowired
+    private Environment environment;
 
+    public static void main(String[] args) {
+        SpringApplication.run(TokyotrainnowApplication.class, args);
+    }
+
+    @EventListener
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        log.info(String.format("Active profiles: %s", Arrays.asList(environment.getActiveProfiles())));
+    }
 }
